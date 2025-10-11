719b82fbe376e2e2abc29aeaffa1db20c67da0f0
Daylon C Pinto <dayloncpinto@gmail.com>
2025-10-11 16:45:25 -0300

    Finaliza settings.py com lógica para dev e prod

--- core/settings.py
+++ core/settings.py
@@ -102,16 +102,27 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+# Lógica inteligente de Banco de Dados
+if os.getenv('DEBUG') == 'False':
+    # --- CONFIGURAÇÃO DE PRODUÇÃO (Lê do .env) ---
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+        }
     }
-}
+else:
+    # --- CONFIGURAÇÃO DE DESENVOLVIMENTO (Usa SQLite local) ---
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
